6400716e816ee4207599910131f9bc59eb42236b
Amilton Santos <a1000ton1000ton@gmail.com>
2025-11-11 17:17:24 -0300

    :fire: ajustando o layout tela pix

--- pages/pix.jsx
+++ pages/pix.jsx
@@ -9,30 +9,8 @@ const Pix = () => {
 
   return (
     <div className="pixContainer">
-      <div className="divPix" onClick={toastSuccess}>
-        <p>Pix Copia e Cola: <b>(62) 99252-9127</b></p>
-        <ContentCopyIcon className="corIconPix" onClick={() => { navigator.clipboard.writeText(numeroPagamento) }} />
-      </div>
-      <br></br>
-      <textarea
-        id="name"
-        style={{
-          width: '95%',
-          height: '100px',
-          border: '1px solid #ccc',
-          borderRadius: '4px',
-          textAlign: 'center',
-          padding: '3px'
-        }}
-        defaultValue='00020126360014br.gov.bcb.pix0114+55629925291275204000053039865802BR5913LIDIANE SILVA6015APARECIDA DE GO62170513CasaEdificada630482B9'
-      />
-      <br></br>
-      <h4> LIDIANE SILVA <span style={{ color: 'red' }}></span></h4>
-      <h4> CAIXA ECONOMICA </h4>
-      <br></br>
-      <hr />
       <img src="/taxaInscricao.jpg" className="pixTaxa" alt="" />
-      
+
       <ToastContainer
         position="bottom-left"
         autoClose={2500}
@@ -43,6 +21,30 @@ const Pix = () => {
         pauseOnFocusLoss
         draggable
         pauseOnHover
+      />
+     
+      <hr />
+
+      <br></br>
+
+      <ContentCopyIcon className="corIconPix" onClick={() => { navigator.clipboard.writeText(numeroPagamento) }} />
+      <div className="divPix" onClick={toastSuccess}>
+        <p>Pix Copia e Cola: <b> (62) 99252-9127 | LIDIANE SILVA CAIXA ECONOMICA</b></p>
+      </div>
+
+      <br></br>
+
+      <textarea
+        id="name"
+        style={{
+          width: '95%',
+          height: '70px',
+          border: '1px solid #ccc',
+          borderRadius: '4px',
+          textAlign: 'center',
+          padding: '3px'
+        }}
+        defaultValue='00020126360014br.gov.bcb.pix0114+55629925291275204000053039865802BR5913LIDIANE SILVA6015APARECIDA DE GO62170513CasaEdificada630482B9'
       />
 
     </div>
